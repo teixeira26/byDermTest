b23c2bf0abfd2cf8113ebc0aaaf621605caf8640
Mathzilla <teixeira.matheus26@gmail.com>
2024-03-23 23:08:26 -0300

    change url to https

--- client/app/models/urls.ts
+++ client/app/models/urls.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'http://104.131.67.42:3001/';
+const BACKEND_URL = 'https://bonodigital.byderm.com.ar/';
 // const BACKEND_URL = 'http://localhost:3001/';
 const BOT_URL = 'http://167.99.126.56:5010/'
 
